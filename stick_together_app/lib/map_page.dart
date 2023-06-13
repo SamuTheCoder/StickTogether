eed2daa8d4b38673fb43ece98cd4de6927165486
Adalberto Junior <adalberton2ta@gmail.com>
2023-06-13 18:18:17 +0100

    Deu certo--> pin e notas estão a funcionar

--- stick_together_app/lib/map_page.dart
+++ stick_together_app/lib/map_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:stick_together_app/models/NoteModel.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -10,7 +13,73 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  List<Marker> markers = [];
+
   @override
+  void initState() {
+    super.initState();
+    loadMarkers();
+  }
+
+  Future<void> loadMarkers() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      final userId = currentUser.uid;
+      final notes = await getNotesFromCurrentUser(userId);
+
+      final updateMarkers = await Future.wait(notes.map((note) async {
+        final username = await getUserName(note.userId);
+
+        return Marker(
+            point: LatLng(note.location.latitude, note.location.longitude),
+            builder: (context) => GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (context) => Column(
+                            children: [
+                              Text(note.note),
+                              SizedBox(
+                                height: 2,
+                              ),
+                              Text(username)
+                            ],
+                          ));
+                },
+                child: Icon(Icons.pin_drop, color: Colors.red)));
+      }).toList());
+      setState(() {
+        markers = updateMarkers;
+        debugPrint("${markers.length}");
+      });
+    }
+  }
+
+  Future<List<NoteModel>> getNotesFromCurrentUser(String userId) async {
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('note')
+        .where('user_id', isEqualTo: userId)
+        .get();
+
+    final notes = querySnapshot.docs.map((doc) {
+      final data = doc.data();
+      return NoteModel(
+          note: data['note'],
+          location: data['location'],
+          expireTime: (data['expire_time'] as Timestamp).toDate(),
+          userId: data['user_id']);
+    }).toList();
+
+    return notes;
+  }
+
+  Future<String> getUserName(String userId) async {
+    final doc =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    final data = doc.data();
+    return data!['username'];
+  }
+
   Widget build(BuildContext context) {
     return Container(
       child: Column(
@@ -26,6 +95,9 @@ class _MapPageState extends State<MapPage> {
                 TileLayer(
                   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                   userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                ),
+                MarkerLayer(
+                  markers: markers,
                 )
               ],
             ),
--- stick_together_app/lib/search.dart
+++ stick_together_app/lib/search.dart
@@ -5,6 +5,7 @@ import 'package:stick_together_app/components/tags_list.dart';
 import 'package:stick_together_app/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:stick_together_app/profile.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -15,25 +16,28 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   TextEditingController seachtf = TextEditingController();
+  void _navagato() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => ProfilePage()));
+  }
+
   @override
   Widget build(BuildContext context) {
     final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
         .collection('users')
         .where(
           'username',
-          isEqualTo: seachtf.text,
+          isEqualTo: seachtf.text.trim(),
         )
         .snapshots();
-    return Scaffold(
+    return Material(
+        child: Scaffold(
       appBar: AppBar(
-        
         title: Container(
-        
           padding: const EdgeInsets.only(
             left: 20,
             right: 10,
           ),
-          
           child: TextField(
             controller: seachtf,
             decoration: const InputDecoration(
@@ -69,6 +73,9 @@ class _SearchPageState extends State<SearchPage> {
                     title: Text(
                       snapshot.data!.docChanges[index].doc['username'],
                     ),
+                    onTap: () {
+                      // _navagato();
+                    },
                   ),
                 );
               },
@@ -76,6 +83,6 @@ class _SearchPageState extends State<SearchPage> {
           );
         },
       ),
-    );
+    ));
   }
 }
